aac459831ca65b56e76879f5f679dc94815e2377
Viktor M <vityok@gmail.com>
2018-02-14 20:45:21 +0200

    EDIT: some improvements here and there...

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ const strToTitle = str => {
   return parts.map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
 }
 
+const splitAndTrim = str => str.split(',').map(x => x.trim()).filter(Boolean);
+
 const FormField = props => (
-  <div className="form-group">
-    <label>{strToTitle(props.attr.replace('_raw', ''))}</label>
-    { props.form.attributesTypes[props.attr] === 'text' ?
-        <input value={props.form.state[props.attr]} onChange={props.form.changeValue} 
-          type="text" className="form-control" 
-          attr={props.attr.replace('_raw', '')} placeholder={'Enter ' + props.attr.replace('_raw', '')} 
-        />
-      :
-        <select value={props.form.state[props.attr]} className="form-control" onChange={props.form.changeValue}
-          attr={props.attr.replace('_raw', '')} >
-            {props.form.options[props.attr].map(x => <option value={x}>{x}</option>)}
-        </select>
-    }
+  <div className="form-group row">
+    <label for={props.attr} className="col-sm-3 col-form-label">{strToTitle(props.attr.replace('_raw', ''))}</label>
+    <div className="col-sm-8">
+      { props.form.attributesTypes[props.attr] === 'text' ?
+          <input value={props.form.state[props.attr]} onChange={props.form.changeValue} 
+            type="text" className="form-control" id={props.attr} key={props.attr}
+            attr={props.attr.replace('_raw', '')} placeholder={'Enter ' + props.attr.replace('_raw', '')} 
+          />
+        :
+          <select value={props.form.state[props.attr]} className="form-control" onChange={props.form.changeValue}
+            attr={props.attr.replace('_raw', '')} key={props.attr.replace('_raw', '')}>
+              {props.form.options[props.attr].map(x => <option key={x} value={x}>{x}</option>)}
+          </select>
+      }
+    </div>
   </div>
 )
 
@@ -30,13 +34,13 @@ class App extends Component {
     this.attributes = [ // To track order 
       'name',
       'rating',
-      'genre',
+      'genres_raw',
       'actors_raw',
       'recommendation'
     ]
     this.attributesTypes = { // To track type of attribute
       name: 'text',
-      genre: 'text',
+      genres_raw: 'text',
       actors_raw: 'text',
       rating: 'select',
       recommendation: 'select'
@@ -52,9 +56,10 @@ class App extends Component {
     // Default state
     this.state = {
       name: props.name || '',
-      genre: props.genre || '',
+      genres_raw: props.genres || '',
+      genres: props.genres ? splitAndTrim(props.genres).map(strToTitle) : [],
       actors_raw: props.actors || '',
-      actors: props.actors ? props.actors.split(',').map(x => x.trim()).filter(Boolean).map(strToTitle) : [],
+      actors: props.actors ? splitAndTrim(props.actors).map(strToTitle) : [],
       rating: this.options.rating.includes(props.rating) ? props.rating : 'so-so',
       recommendation: this.options.recommendation.includes(props.recommendation) ? props.recommendation : 'movie experts',
     };
@@ -62,11 +67,11 @@ class App extends Component {
 
   changeValue = function (event) {
     let newState = {};
-    // console.log(event.target);
+    console.log(event.target);
     const attr = event.target.getAttribute('attr');
-    if (attr === 'actors') {
-      newState['actors_raw'] = event.target.value;
-      newState['actors'] = event.target.value.split(',').map(x => x.trim()).filter(Boolean).map(strToTitle);
+    if (this.attributes.includes(`${attr}_raw`)) {
+      newState[`${attr}_raw`] = event.target.value;
+      newState[attr] = event.target.value.split(',').map(x => x.trim()).filter(Boolean).map(strToTitle);
     } else {
       newState[attr] = event.target.value;
     }
@@ -84,7 +89,7 @@ class App extends Component {
           </div>
           <div className="card-body">
             <p>Rating: <strong>{this.state.rating}</strong></p>
-            <p>Genre: <strong>{this.state.genre}</strong></p>
+            <p>Genres: {this.state.genres.map(x => (<span className="badge badge-primary spacey">{x}</span>))}</p>
             Actors: {this.state.actors.length ? (<ul>{this.state.actors.map(a => <li>{a}</li>)}</ul>): (<i>No actors</i>) }
             <p>Would recommend to: <strong>{this.state.recommendation}</strong></p>
           </div>
@@ -92,7 +97,7 @@ class App extends Component {
             <h5 className="card-title">Edit form</h5>
             <form>
               {/* Text-based inputs go first */}
-              {this.attributes.sort(x => this.attributesTypes[x] === 'select').map(x => (<FormField attr={x} form={this}/>))}
+              {this.attributes.map(x => (<FormField attr={x} form={this}/>))}
             </form>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App name="Pulp Fiction" rating="super" genre="action" actors="John Travolta, Uma Thurman, Samuel L. Jackson " />, document.getElementById('root'));
+ReactDOM.render(<App name="Pulp Fiction" rating="super" genres="action, comedy" actors="John Travolta, Uma Thurman, Samuel L. Jackson " />, document.getElementById('root'));
 registerServiceWorker();
